cac133858ad1199ddf3af753d85b158aaed37bd8
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-05 23:52:31 +0300

    String representation of the rectangle with #

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -73,6 +73,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         rectangle = ''
-        for i in range(self.__height):
-            rectangle += "#" * self.__width + "\n"
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
         return rectangle
+
