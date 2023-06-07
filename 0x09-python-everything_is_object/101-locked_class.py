76484f1755c277cfdfd48fd54bc9592e78673901
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-07 20:31:00 +0300

    fixing errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""module that contains a LockedClass"""
+
 
 class LockedClass:
+    """
+    LockedClass prevents the creation of new instance attributes,
+    except for the attribute 'first_name'
+    """
     __instance__ = ["first_name"]
 
     def __setattr__(self, name, value):
+        """
+        Custom attribute method that allow assigning of
+        the 'first_name' attribute and raises an error for any
+        other attribute assignment.
+        """
+        msg = "'LockedClass' object has no attribute 'last_name'"
         if name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
+            raise AttributeError(msg)
         self.__dict__[name] = value
