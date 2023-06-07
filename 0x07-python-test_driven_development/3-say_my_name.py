49a691661d696e2aae3475a7ec04db1528bbd94b
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-07 13:25:06 +0300

    fixing errors

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,17 @@ that prints name
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the second name
+
+    Raises:
+        TypeError: if either the first_name and last name
+                    is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
@@ -14,4 +25,4 @@ def say_my_name(first_name, last_name=""):
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
     else:
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(first_name))
