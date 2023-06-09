0506be82682840948bed1d5ddcb6aa4c6dc775eb
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-09 20:48:11 +0300

    solving the N queens problem

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,77 +3,51 @@
 import sys
 
 
-def is_safe(board, row, col):
-    # Check if it is safe to place a queen at board[row][col]
-
-    # Check the left side of the current row
-    for i in range(col):
-        if board[row][i] == 'Q':
+def is_safe(board, row, col, N):
+    # Check if a queen can be placed at the given position
+    for i in range(row):
+        if board[i] == col or board[i] - col == i - row or board[i] - col == row - i:
             return False
-
-    # Check the upper diagonal
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 'Q':
-            return False
-        i -= 1
-        j -= 1
-
-    # Check the lower diagonal
-    i = row
-    j = col
-    while i < N and j >= 0:
-        if board[i][j] == 'Q':
-            return False
-        i += 1
-        j -= 1
-
     return True
 
 
-def solve_nqueens(board, col):
-    # Recursive function to solve the N-Queens problem
-
-    # Base case: If all queens are placed, print the solution
-    if col >= N:
-        print_solution(board)
+def solve_nqueens(board, row, N, solutions):
+    # Base case: All queens have been placed
+    if row == N:
+        solutions.append([(i, board[i]) for i in range(N)])
         return
 
-    for i in range(N):
-        if is_safe(board, i, col):
-            board[i][col] = 'Q'  # Place the queen at board[i][col]
-            solve_nqueens(board, col + 1)  # Recur for the next column
-            board[i][col] = '.'  # Backtrack and remove the queen
+    # Recursive case: Try placing a queen in each column of the current row
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_nqueens(board, row + 1, N, solutions)
 
 
-def print_solution(board):
-    # Utility function to print the board configuration
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=' ')
-        print()
-    print()
+def print_solutions(N):
+    # Check if N is smaller than 4
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+    solve_nqueens(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
 
 
-# Check if the number of arguments is correct
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if __name__ == '__main__':
+    # Check if the program was called with the correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens.py N")
+        sys.exit(1)
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
-# Check if N is at least 4
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-# Initialize an empty chessboard
-board = [['.' for _ in range(N)] for _ in range(N)]
-
-# Solve the N-Queens problem
-solve_nqueens(board, 0)
+    print_solutions(N)
