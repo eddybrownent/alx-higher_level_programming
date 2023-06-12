169ea6111fbc752de2998e0af1a1a818dcf0b74a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-12 13:47:45 +0300

    fixing errors

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,5 +9,12 @@ from the specified class
 def inherits_from(obj, a_class):
     """
     checks if the object's type is a subclass of the specified class,
+
+    Args:
+        obj (object): the object to be checked
+        a_class (class): the class to check in
+
+    Return:
+        bool: True if the object is an instance of a class else false
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
