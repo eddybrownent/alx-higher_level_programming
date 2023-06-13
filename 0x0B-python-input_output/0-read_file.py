1899b0ec71a93438bfc54b40d83179eb2be59c35
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-13 15:46:05 +0300

    fixing output errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,4 +15,4 @@ def read_file(filename=""):
     """
     with open(filename, "r", encoding="utf-8") as a_file:
         content = a_file.read()
-        print(content)
+        print(content, end="")
