0856305c1e3a5b9ef10a0e69601b2b16252e1c1c
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-14 16:29:09 +0300

    fixing the code style to fit semistandard requirements

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,9 +6,9 @@ if (!isNaN(arg)) {
   const parsedArg = parseInt(arg);
   if (Number.isInteger(parsedArg)) {
     console.log(`My number: ${parseInt(arg)}`);
-} else {
-  console.log('Not a number');
-}
+  } else {
+    console.log('Not a number');
+  }
 } else {
   console.log('Not a number');
 }
