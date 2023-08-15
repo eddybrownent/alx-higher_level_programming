64d56c6e9bd8eab232c0a7ddc54ddca3cf2e87ce
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-15 12:42:02 +0300

    fixing the codestyle so a to meet the code requirements

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-//If w or h is <= 0  create an empty object
+// If w or h is <= 0  create an empty object
 module.exports = class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-     this.width = w;
-     this.height = h;
+      this.width = w;
+      this.height = h;
+    }
   }
-}
 };
