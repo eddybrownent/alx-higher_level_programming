e09c3482c3d2547f3ab4d2691681cd0434998769
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-15 12:39:27 +0300

    changing some logic to get the correct output required

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
+//If w or h is <= 0  create an empty object
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+     this.width = w;
+     this.height = h;
   }
+}
 };
