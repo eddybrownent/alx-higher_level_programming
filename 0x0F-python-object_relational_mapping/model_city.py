0e121dc73f663a91a705325476057e263d0a7f46
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-20 19:28:17 +0300

    added model that creates a city table that contains the class definition of a City  and that prints all City objects from the database

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pyhon3
+"""
+This script defines a City class for
+SQALchemy ORM
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    City class representing a city in the database
+    Inherits from the Base class from model_state
+    """
+
+    __tablename__ = 'cities'
+
+    # Columns definition
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
