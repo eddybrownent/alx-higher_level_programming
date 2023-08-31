5561cb9461d36a0040377c7e91ef3cea448e4ed4
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-01 00:04:07 +0300

    changed the output

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URl and sends a POST request to it and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject= I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
