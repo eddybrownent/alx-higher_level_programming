444c1da825a44fb62cf26d33346076696f9c40af
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-31 13:45:14 +0300

    fixing the first line of code to required

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script that takes URL sends a GET request to the URL and displays the body of the response
 curl -sL -w "%{http_code}" -o /dev/null "$1" | grep -q 200 && curl -sL "$1"
