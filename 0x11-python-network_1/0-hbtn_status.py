1ebebf38225ab4612992c04d338f377636524143
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-01 12:37:52 +0300

    changed the doc style and added a script  that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-# Script that fetches https://alx-intranet.hbtn.io/status
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
-    # Use a with statement to open the URL
+    """ Use a with statement to open the URL """
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as html:
         content = html.read()
 
-        # Print a header for the info
+        """ Print a header for the info """
         print("Body response:")
 
-        # print the type of the contents
+        """ print the type of the contents """
         print("     - type: {}".format(type(content)))
 
-        # print the content of the response
+        """ print the content of the response """
         print("     - content: {}".format(content))
 
-        # decodes the content "UTF-8" and print it
+        """ decodes the content "UTF-8" and print it """
         print("     - utf8 content: {}".format(content.decode("utf-8")))
