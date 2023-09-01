4303e05c54e4bc6156406647964771a0aa898d93
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-01 16:51:38 +0300

    added a condition to make sure the  code should not be executed when imported

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,18 @@ with the letter as a parameter
 import requests
 import sys
 
-
-if len(sys.argv) == 1:
-    payload = {'q': ""}
-else:
-    payload = {'q': sys.argv[1]}
-response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-parsed = response.json()
-
-try:
-    if parsed == {}:
-        print("No result")
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        payload = {'q': ""}
     else:
-        print("[{}] {}".format(parsed.get('id'), parsed.get('name')))
-except ValueError:
-    print("Not a valid JSON")
+        payload = {'q': sys.argv[1]}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    parsed = response.json()
+
+    try:
+        if parsed == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(parsed.get('id'), parsed.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
